b0c89a910df7742ed07a5f973396a526ed44709d
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-02-21 11:22:01 +0900

    fix: use io.Reader and Writer

--- ex00/main.go
+++ ex00/main.go
@@ -2,24 +2,47 @@ package main
 
 import (
 	"bufio"
+	"io"
 	"os"
+	"strings"
 )
+
+func readWrite(r io.Reader, w io.Writer) error {
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		_, err := io.WriteString(w, scanner.Text()+"\n")
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
 
 func main() {
 	args := os.Args
 	if len(args) == 1 {
-		scanner := bufio.NewScanner(os.Stdin)
-		for scanner.Scan() {
-			os.Stdout.WriteString(scanner.Text() + "\n")
+		if err := readWrite(os.Stdin, os.Stdout); err != nil {
+			os.Stderr.WriteString(err.Error())
 		}
-		os.Exit(0)
-	}
-	for _, filePath := range args[1:] {
-		content, err := os.ReadFile(filePath)
-		if err != nil {
-			os.Stderr.WriteString("cat: " + filePath + ": No such file or directory\n")
-			continue
+	} else {
+		for _, filePath := range args[1:] {
+			finfo, err := os.Stat(filePath)
+			if err != nil {
+				os.Stderr.WriteString(err.Error())
+			}
+			if finfo.IsDir() {
+				os.Stderr.WriteString("ft_cat: " + filePath + ": Is a directory\n")
+				continue
+			}
+			f, err := os.Open(filePath)
+			if err != nil {
+				os.Stderr.WriteString("ft_cat: " + strings.TrimPrefix(err.Error(), "open ") + "\n")
+				continue
+			}
+			defer f.Close()
+			if err := readWrite(f, os.Stdout); err != nil {
+				os.Stderr.WriteString(err.Error())
+			}
 		}
-		os.Stdout.WriteString(string(content))
 	}
 }
