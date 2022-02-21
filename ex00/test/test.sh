d2bb612407234f9df76d929358c1ff5c55217958
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-02-21 11:30:03 +0900

    add: test case

--- ex00/test/test.sh
+++ ex00/test/test.sh
@@ -13,5 +13,16 @@ echo "test: START"
 ASSERT "$(../ft_cat test1.txt)" "$(cat test1.txt)"
 ASSERT "$(../ft_cat test2.txt)" "$(cat test2.txt)"
 ASSERT "$(../ft_cat test1.txt test2.txt)" "$(cat test1.txt test2.txt)"
+ASSERT "$(echo \"hoge\" | ../ft_cat)" "$(echo \"hoge\" | cat)"
+ASSERT "$(../ft_cat << EOS
+hoge
+huga
+EOS
+)" "$(cat << EOS
+hoge
+huga
+EOS
+)"
+ASSERT "$(../ft_cat < test1.txt)" "$(cat < test1.txt)"
 
 echo "test: OK"
